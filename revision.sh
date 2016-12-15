775118eb5e7548685be207831e0aaad923fe8416
mylxsw <mylxsw@aicode.cc>
2016-12-15 16:06:21 +0800

    update at 20161215

---
+++ revision.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cat README.md > VERSION
+echo $(date) >> VERSIO
+cat README.md >> VERSION
+cat README.md >> VERSION
+echo $(date) >> VERSION
+cat README.md >> VERSION
+cat README.md >> VERSION
+echo $(date) >> VERSION
+cat README.md >> VERSION
+cat README.md >> VERSION
+echo $(date) >> VERSION
+cat README.md >> VERSION
+cat README.md >> VERSION
+echo $(date) >> VERSION
+cat README.md >> VERSION
+cat README.md >> VERSION
+echo $(date) >> VERSION
+
+cat VERSION >> VERSION
+cat VERSION >> VERSION
+cat VERSION >> VERSION
+cat VERSION >> VERSION
+cat VERSION >> VERSION
+
+git status
+git add --all
+git commit -m "update at $(date +%Y%m%d)"
+git push
